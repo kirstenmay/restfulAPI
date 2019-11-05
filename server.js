37b34c784149ca1560ecdfc4984093f4625f0ac2
Kirsten Lindblom <kirsten.m.lindblom@gmail.com>
2019-11-05 15:40:17 -0800

    Added angular app

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/name_of_your_DB', { useNewUrlParser: true });
 const app = express();
-app.use(express.static(__dirname + "/static"));
+app.use(express.static(__dirname + '/public/dist/public'));
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
